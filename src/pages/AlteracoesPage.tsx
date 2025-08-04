e0a2ece3f573e117e9dbcd991fc33c0078ce7919
Yuri Odilon <moura.n.yuri@gmail.com>
2025-08-04 11:23:07 -0400

    feat: página de alterações finalizada

--- src/pages/AlteracoesPage.tsx
+++ src/pages/AlteracoesPage.tsx
@@ -13,9 +13,10 @@ interface IHistorico {
 export default function AlteracoesPage() {
     const [logs, setLogs] = React.useState<IHistorico[] | null>(null)
     const [filterOptions, setFilterOptions] = React.useState({
-        checkbox: 'arquivou',
+        checkbox: 'Arquivar',
         search: ''
     })
+    const { checkbox, search } = filterOptions
 
     React.useEffect(() => {
         const fetchData = async () => {
@@ -29,6 +30,26 @@ export default function AlteracoesPage() {
         fetchData()
     }, [])
 
+    const filterLogs = (): IHistorico[] | undefined => {
+        if (logs) {
+            let filteredListOfLogs = logs
+
+            if (checkbox) {
+                filteredListOfLogs = filteredListOfLogs?.filter((log) => {
+                    return log.acao === checkbox
+                })
+            }
+
+            if (search) {
+                filteredListOfLogs = filteredListOfLogs?.filter((log) => {
+                    return log.mensagem.includes(search)
+                })
+            }
+
+            return filteredListOfLogs
+        }
+    }
+
     return (
         <main className="flex flex-col gap-5 py-5 pr-10 max-h-[824px]">
             <h1 className="text-4xl text-sky-950 font-semibold pb-8">Histórico de Alterações</h1>
@@ -37,7 +58,7 @@ export default function AlteracoesPage() {
                 setFilterOptions={setFilterOptions}
             />
             <section className="flex flex-col gap-4 bg-gray-100 p-4 overflow-y-scroll rounded">
-                {logs?.map(({ id, mensagem }) => (
+                {filterLogs()?.map(({ id, mensagem }) => (
                     <div className="bg-slate-300 p-3 rounded" key={id}>
                         <p className="font-medium text-lg">
                             {mensagem}
