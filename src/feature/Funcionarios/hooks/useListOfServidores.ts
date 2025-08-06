2de02c3b9dde8d9c3ce8eff45bbff21085c3e053
Yuri Odilon <moura.n.yuri@gmail.com>
2025-08-06 11:46:47 -0400

    feat: estado que mostra se a ação esta ocorrendo exatamente em no card do servidor selecionado

--- src/feature/Funcionarios/hooks/useListOfServidores.ts
+++ src/feature/Funcionarios/hooks/useListOfServidores.ts
@@ -4,9 +4,9 @@ import { toast } from "sonner"
 import { api } from "@/api/axios"
 
 export default function useListOfServidores(
-    servidores: IServidor[] | null, 
-    search: string, 
-    setIsLoading: React.Dispatch<React.SetStateAction<boolean>>
+    servidores: IServidor[] | null,
+    search: string,
+    setIsLoading: React.Dispatch<React.SetStateAction<{ id: number | null, load: boolean, action: string | null }>>
 ) {
     const storedUser = JSON.parse(localStorage.getItem('user')!) as User
 
@@ -37,13 +37,14 @@ export default function useListOfServidores(
 
     const handleArchiveServidor = async (id: number) => {
         try {
-            setIsLoading(true)
+            setIsLoading({ id: id, load: true, action: 'arquivar' })
 
             const response = await api.patch(`/servidores/${id}/arquivar`)
             const { servidor_arquivado } = await response.data as { mensagem: string, servidor_arquivado: { nome: string, setor: string } }
 
             await historyLogsArchive(storedUser.nome, servidor_arquivado.nome, servidor_arquivado.setor)
 
+            setIsLoading({ id: null, load: false, action: null })
             toast.success('Servidor arquivado com sucesso')
             setTimeout(() => {
                 window.location.reload()
@@ -52,7 +53,7 @@ export default function useListOfServidores(
             console.error("Error ao arquivar servidor", error)
             toast.error('Não foi possível arquivar o servidor')
         } finally {
-            setIsLoading(false)
+            setIsLoading((prevValues) => ({ ...prevValues, load: true }))
         }
     }
 
@@ -70,13 +71,14 @@ export default function useListOfServidores(
 
     const handleActiveServidor = async (id: number) => {
         try {
-            setIsLoading(true)
+            setIsLoading({ id: id, load: true, action: 'desarquivar' })
 
             const response = await api.patch(`/servidores/${id}/atualizar-status`)
             const { servidor_ativado } = response.data as { mensagem: string, servidor_ativado: { nome: string, setor: string } }
 
             await historyLogsUnarchive(storedUser.nome, servidor_ativado.nome, servidor_ativado.setor)
 
+            setIsLoading({ id: null, load: false, action: null })
             toast.success('Servidor desarquivado com sucesso')
             setTimeout(() => {
                 window.location.reload()
@@ -85,13 +87,59 @@ export default function useListOfServidores(
             console.error("Error ao desarquivar servidor", error)
             toast.error('Não foi possível desarquivar o servidor')
         } finally {
-            setIsLoading(false)
+            setIsLoading((prevValues) => ({ ...prevValues, load: false }))
+        }
+    }
+
+    const downloadFichaFuncional = async (documento_id: number) => {
+        try {
+            const response = await api.get(`/fichas-funcionais/download/${documento_id}`, {
+                responseType: 'blob'
+            });
+
+            const url = window.URL.createObjectURL(new Blob([response.data]))
+            const link = document.createElement('a')
+            link.href = url
+
+            const contentDisposition = response.headers['content-disposition']
+            let fileName = 'ficha-funcional.pdf'
+
+            if (contentDisposition) {
+                const fileNameMatch = contentDisposition.match(/filename="?(.+)"?/)
+                if (fileNameMatch?.[1]) {
+                    fileName = fileNameMatch[1]
+                }
+            }
+
+            link.setAttribute('download', fileName)
+            document.body.appendChild(link)
+            link.click()
+            link.remove()
+
+        } catch (error) {
+            console.error('Erro ao baixar o arquivo:', error)
+        }
+    }
+
+    const generateFichaFuncional = async (id: number) => {
+        try {
+            setIsLoading({ id: id, load: true, action: 'ficha' })
+
+            const response = await api.post(`servidores/${id}/gerar-ficha-funcional`)
+            const { documento_id } = response.data
+
+            await downloadFichaFuncional(documento_id)
+            setIsLoading({ id: null, load: false, action: null })
+        } catch (error) {
+            console.log('Erro ao gerar ficha funcional', error)
+            toast.error('Não foi possível gerar a ficha funcional')
         }
     }
 
     return {
         handleActiveServidor,
         handleArchiveServidor,
+        generateFichaFuncional,
         filterServidores
     }
 }
